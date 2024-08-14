1cc19fcdee2ec4c1b1e7d32489d127a6a8433411
Ho Hoang Phuoc <69688044+hhoangphuoc@users.noreply.github.com>
2024-08-14 16:52:55 +0200

    added training file for model running

--- TimbreTransfer/T2R2D2/T2R2D2.py
+++ TimbreTransfer/T2R2D2/T2R2D2.py
@@ -1,108 +1,156 @@
-def main():
-    #TODO: FIX THE ARGUMENTS TO FIT THE DATASET (URMP and R2D2)
-    parser = argparse.ArgumentParser(description='Train log-mel-to-mask network')
-    parser.add_argument('--dataset_train_path', type=str, help='Folder containing Train/val Dataset audio',
-                        default='dataset/starnet/starnet_reduced')
-    parser.add_argument('--desired_instrument', type=str, help='Desired Output Timbre',
-                        default='strings')
-    parser.add_argument('--conditioning_instrument', type=str, help='Desired Conditioning Timbre',
-                        default='clarinet')
-    parser.add_argument('--GPU', type=str, help='Select GPU number',
-                        default='0')
-    parser.add_argument('--train', type=str, help='Select GPU number',
-                        default='True')
+#TODO: The main file to execute the model
+import os
+import tqdm
+import numpy as np
+import tensorflow as tf
+import datetime
 
+from tensorflow import keras
+import argparse
 
-    dict_instruments = {"clarinet":"1","vibraphone":"2","strings":"4","piano":"5",'clarinet_vibraphone':"0",'strings_piano':"3"}
+import model
+import params.audio_params as aprs
+import params.model_params as mprs
+from preprocess.dataset_prep import prepare_spectrograms
+
+def prepare_data(data_path, tgt_timbre, cond_timbre):
+    # load the spectrograms from the dataset
+    tgt_data_path = os.path.join(args.data_path, tgt_timbre)
+    cond_data_path = os.path.join(args.data_path, cond_timbre)
+    tgt_specs_path = os.path.join(tgt_data_path, 'tgt_specs.npy')
+    cond_specs_path = os.path.join(cond_data_path, 'cond_specs.npy')
+
+    tgt_specs = None
+    cond_specs = None
     
-    args = parser.parse_args()
+    #check if the dataset is already preprocessed
+    if not os.path.exists(tgt_specs_path) and not os.path.exists(cond_specs_path):
+        print("Dataset not preprocessed, preprocessing...")
+        tgt_specs, cond_specs = prepare_spectrograms(tgt_data_path, cond_data_path)
+    else:
+        print("Loading spectrograms...")
+        tgt_specs = np.load(tgt_specs_path)
+        cond_specs = np.load(cond_specs_path)
 
-    desired_instrument = args.desired_instrument
-    conditioning_instrument = args.conditioning_instrument
-    dataset_train_path = args.dataset_train_path
-    train = args.train
-    print('Timbre transfering from '+conditioning_instrument+' to'+desired_instrument)
+    val_perc = 0.2
+    n_samples = len(tgt_specs)
+    n_train = int(n_samples * (1 - val_perc))
+    
+    rng = np.random.default_rng(12345)
+    idxs = rng.permutation(n_samples)
+    train_idxs = idxs[:n_train]
+    val_idxs = idxs[n_train:]
 
-    # Handle Paths
-    instruments_name = [dict_instruments[desired_instrument],dict_instruments[conditioning_instrument]]
+    train_tgt_specs = tgt_specs[train_idxs]
+    train_cond_specs = cond_specs[train_idxs]
+    val_tgt_specs = tgt_specs[val_idxs]
+    val_cond_specs = cond_specs[val_idxs]
+    
+    return train_tgt_specs, train_cond_specs, val_tgt_specs, val_cond_specs
+    
+    # return tgt_specs, cond_specs
 
-    checkpoint_path = "checkpoints/ATT_STARNET_NORM_diffusion_model_timbre_transfer_"+conditioning_instrument+'_to_'+desired_instrument+'_'+ datetime.datetime.now().strftime(
-        "%Y%m%d-%H%M%S")
-    # tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir="./logs/ATT_STARNET_"+conditioning_instrument+'_to_'+desired_instrument+datetime.datetime.now().strftime(
-    #     "%Y%m%d-%H%M%S"))
+def create_tf_dataset(tgt_specs, cond_specs, batch_size, training=True):
+    dataset = tf.data.Dataset.from_tensor_slices((tgt_specs, cond_specs))
+    if training:
+        dataset = dataset.shuffle(buffer_size=1000)
+    dataset = dataset.batch(batch_size).prefetch(tf.data.experimental.AUTOTUNE)
+    return dataset
 
-    log_dir = 'logs/'
-    logdir = log_dir + 'ATT_STARNET_NORM_diffusion_timbre_transfer_' + datetime.datetime.now().strftime(
-        "%Y%m%d-%H%M%S") + '5000__'+conditioning_instrument+'_to_'+desired_instrument
+def train_model(train_tgt_specs, train_cond_specs, val_tgt_specs, val_cond_specs, log_path, checkpoint_path, lr, weight_decay, batch_size, epochs):
 
+    # Prepare datasets
+    train_dataset = create_tf_dataset(train_tgt_specs, train_cond_specs, batch_size, training=True)
+    val_dataset = create_tf_dataset(val_tgt_specs, val_cond_specs, batch_size, training=False)
 
-    # TODO: REPLACE WITH THE CUSTOM PATH AND PARSING TO THE RIGHT PLACE IN INPUT
-    # Each instrument is the same since track names are duplicatedk
-
-    # tracks_full = os.listdir(dataset_train_path)
-    # cond_tracks = [track for track in tracks_full if track.split('.')[-2]==instruments_name[1]]
-    # trgt_tracks = [track for track in tracks_full if track.split('.')[-2]==instruments_name[0]]
-    # cond_tracks.sort()
-    # trgt_tracks.sort()
-    # track_paths_trans = [[os.path.join(dataset_train_path,trgt_tracks[i]),os.path.join(dataset_train_path,cond_tracks[i])] for i in range(len(cond_tracks))]
-
-
-    # val_perc = 0.2
-    # n_tracks_train = len(track_paths_trans) - int(np.floor(val_perc * len(track_paths_trans)))
-    # rng = np.random.default_rng(12345)
-    # idxs = rng.choice(len(track_paths_trans), len(track_paths_trans), False)
-    # idxs_train = idxs[:n_tracks_train]
-    # idxs_val = idxs[n_tracks_train:]
-
-    # train_tracks_paths = np.array(track_paths_trans)[idxs_train].tolist()
-    # val_tracks_paths = np.array(track_paths_trans)[idxs_val].tolist()
-
-    # train_dataset = prepare_dataset(train_tracks_paths)
-    # val_dataset = prepare_dataset(val_tracks_paths, training=False)
-
-
-    # create and compile the model
-    first = True
-    for a in val_dataset.take(2):
-        if first:
-            val_data = a
-            first = False
-        else:
-            val_data = tf.concat([val_data, a],axis=0)
-    val_data = a[:18]
+    # Prepare validation subset for visualization
+    val_data = next(iter(val_dataset.take(1)))[:18]  # Adjust the number as need
     print(val_data.shape)
-    model = network_lib.DiffusionModel(params.mel_spec_size, params.widths, params.block_depth, val_data, params.has_attention, logdir=logdir,batch_size=params.batch_size,)
-    model.network.summary()
 
-    if train:
-        model.compile(
+    diff_model = model.DiffusionModel(aprs.MEL_SPEC_NORM_SIZE, mprs.WIDTHS, mprs.BLOCK_DEPTH, val_data, mprs.HAS_ATTENTION, log_path, mprs.BATCH_SIZE)
+
+    # train the model
+    diff_model.compile(
             optimizer=keras.optimizers.experimental.AdamW(
-                learning_rate=params.learning_rate, weight_decay=params.weight_decay
+                learning_rate= lr, weight_decay=weight_decay
             ),
             loss=keras.losses.mean_absolute_error,
         )
 
-        # save the best model based on the validation KID metric
-
-        checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+    # save the best model based on the validation KID metric
+    checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
             filepath=checkpoint_path,
             save_weights_only=True,
             monitor="val_n_loss",
             mode="min",
             save_best_only=True,
         )
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir="./logs/T2R2D2_"+args.cond_timbre+'_to_'+args.tgt_timbre+datetime.datetime.now().strftime(
+        "%Y%m%d-%H%M%S"))
+    
+    # calculate mean and variance of training dataset for normalization
+    diff_model.fit(
+        train_dataset,
+        epochs=epochs, #default 5000
+        validation_data=val_dataset,
+        callbacks=[
+            keras.callbacks.LambdaCallback(on_epoch_end=diff_model.plot_images),
+            checkpoint_callback,
+            tensorboard_callback,
+        ],
+    )
+    return diff_model
 
-        # calculate mean and variance of training dataset for normalization
-        model.fit(
-            train_dataset,
-            epochs=5000,
-            validation_data=val_dataset,
-            callbacks=[
-                keras.callbacks.LambdaCallback(on_epoch_end=model.plot_images),
-                checkpoint_callback,
-                tensorboard_callback,
-            ],
-        )
+#----------------------------------------------------------
 
-if __name__=='__main__':
+def main():
+    parser = argparse.ArgumentParser()
+    #parser for input data
+    parser.add_argument('--data_path', type=str, default='./data_specs/' , help='Path to the datasource')
+    parser.add_argument('--model_path', type=str, default='./save_models/' , help='Path to the models')
+    parser.add_argument('--tgt_timbre', type=str, default='r2d2' , help='Name of Target timbre')
+    parser.add_argument('--cond_timbre', type=str, default='vn' , help='Name of Conditioning timbre')
+
+    parser.add_argument('--GPU', type=str, default='0' , help='Number of GPU')
+    parser.add_argument('--train', type=bool, default=False , help='Train the model')
+
+    #parser for model parameters
+    parser.add_argument('--batch_size', type=int, default=mprs.BATCH_SIZE)
+    parser.add_argument('--lr', type=float, default=mprs.LEARNING_RATE)
+    parser.add_argument('--weight_decay', type=float, default=mprs.WEIGHT_DECAY)
+    parser.add_argument('--epochs', type=int, default=mprs.NUM_EPOCHS)
+    
+    parser.add_argument('--ema', type=float, default=mprs.EMA)
+
+    args = parser.parse_args()
+    print(args)
+
+    # tgt_specs, cond_specs = prepare_data(args.data_path, args.tgt_timbre, args.cond_timbre)
+    train_tgt_specs, train_cond_specs, val_tgt_specs, val_cond_specs = prepare_data(args.data_path, args.tgt_timbre, args.cond_timbre)
+
+    checkpoint_path = "checkpoints/T2R2D2_"+args.cond_timbre+'_to_'+args.tgt_timbre+'_'+ datetime.datetime.now().strftime(
+        "%Y%m%d-%H%M%S")
+
+    log_dir = 'logs/'
+    log_path = log_dir + 'T2R2D2_' + datetime.datetime.now().strftime(
+        "%Y%m%d-%H%M%S") + '5000__'+args.cond_timbre+'_to_'+args.tgt_timbre
+
+    
+    if args.train:
+        # trained_model = train_model(tgt_specs, cond_specs, log_path, checkpoint_path, args.lr, args.weight_decay, args.batch_size, args.epochs)
+        # trained_model.save_weights(os.path.join(args.model_path, 'T2R2D2_diff_model.h5'))
+        trained_model = train_model(train_tgt_specs, train_cond_specs, val_tgt_specs, val_cond_specs, 
+                                    log_path, checkpoint_path, args.lr, args.weight_decay, args.batch_size, args.epochs)
+        trained_model.save_weights(os.path.join(args.model_path, 'T2R2D2_diff_model.h5'))
+    else:
+        diff_model.load_weights(os.path.join(args.data_path, 'T2R2D2_diff_model.h5'))
+
+
+if __name__ == "__main__":
     main()
+
+
+
+    
+
+
